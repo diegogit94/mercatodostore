ed34e87deffa1ccfb3854e77770907b7403759bc
Diego Ospina <diego.ospinagp@gmail.com>
2020-07-29 17:04:47 -0500

    feat: configure auth and guest buttons on main-right.blade.php

--- resources/views/partials/menus/main-right.blade.php
+++ resources/views/partials/menus/main-right.blade.php
@@ -1,28 +1,23 @@
 <ul>
-    @guest
-    <li><a href="{{ route('register') }}">Sign Up</a></li>
-    <li><a href="{{ route('login') }}">Login</a></li>
-    @else
-    <li>
-        <a href="{{ route('users.edit') }}">My Account</a>
-    </li>
-    <li>
-        <a href="{{ route('logout') }}"
-            onclick="event.preventDefault();
-                     document.getElementById('logout-form').submit();">
-            Logout
-        </a>
-    </li>
+    @auth
+        @if (Auth::user()->user_type == 'admin')
+            <li><a href="{{ route('users.index') }}">Admin</a></li>
+        @endif
+        <li><a href="#">Shop</a></li>
+        <li><a href="{{ route('home') }}">Home</a></li>
+        <li><a href="#"><img src="/img/cart.png" alt="cart image" width="30mx" height="30mx"></a></li>
 
-    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-        {{ csrf_field() }}
-    </form>
-    @endguest
+    @else
+        
+        <li><a href="{{ route('register') }}">Register</a></li>
+        <li><a href="{{ route('login') }}">Login</a></li>
+    @endauth
+
 {{--    <li><a href="{{ route('cart.index') }}">Cart--}}
 {{--    @if (Cart::instance('default')->count() > 0)--}}
 {{--    <span class="cart-count"><span>{{ Cart::instance('default')->count() }}</span></span>--}}
 {{--    @endif--}}
-    </a></li>
+{{--    </a></li>--}}
     {{-- @foreach($items as $menu_item)
         <li>
             <a href="{{ $menu_item->link() }}">
