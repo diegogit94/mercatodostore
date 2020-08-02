e0cfa0e6853d3ea121c677835c822eac1db451a2
Diego Ospina <diego.ospinagp@gmail.com>
2020-08-02 14:09:27 -0500

    fix: added the fillable and casts variables to Product and Category models for massive assignation and added the observers to the boot function on AppServiceProvider file

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'name', 'slug', 'description',
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $casts =[
+        'email_verified_at' => 'datetime',
+    ];
 }
--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'name', 'slug', 'short_description', 'description', 'image', 'price'
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $casts =[
+        'email_verified_at' => 'datetime',
+    ];
 }
