f5c399ad53b8c986b0c2692bccabef06529f1563
Diego Ospina <diego.ospinagp@gmail.com>
2020-08-02 20:09:15 -0500

    feat: added createProduct view without activate/desactivate buttons

--- resources/views/admin/createProduct.blade.php
+++ resources/views/admin/createProduct.blade.php
@@ -42,7 +42,7 @@
                             </div>
 
                             <div class="form-group row">
-                                <label for="description" class="col-md-4 col-form-label text-md-right" value="{{ old('short_description') }}">{{ __('Descripcion') }}</label>
+                                <label for="description" class="col-md-4 col-form-label text-md-right" value="{{ old('description') }}">{{ __('Descripcion') }}</label>
 
                                 <div class="col-md-6">
                                     <textarea id="description" type="text" class="form-control @error('description') is-invalid @enderror" name="description" required autocomplete="new-description"></textarea>
