c03a6d2108564105a84435867ab9759a3d7b6d14
Diego Ospina <diego.ospinagp@gmail.com>
2020-08-17 13:35:49 -0500

    feat: modified products table migration, added the quantity column and modified the ProductSeeder

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -16,24 +16,28 @@ class ProductSeeder extends Seeder
             'category_id' => '1',
             'name' => 'game1',
             'slug' => 'game-1',
-            'image' => 'public/game1.jpg']);
+            'image' => 'public/game1.jpg',
+            'quantity' => 5]);
 
         factory(Product::class)->create([
             'category_id' => '1',
             'name' => 'game2',
             'slug' => 'game-2',
-            'image' => 'public/game2.jpeg']);
+            'image' => 'public/game2.jpeg',
+            'quantity' => 5]);
 
         factory(Product::class)->create([
             'category_id' => '2',
             'name' => 'game3',
             'slug' => 'game-3',
-            'image' => 'public/game3.jpg']);
+            'image' => 'public/game3.jpg',
+            'quantity' => 5]);
 
         factory(Product::class)->create([
             'category_id' => '2',
             'name' => 'game4',
             'slug' => 'game-4',
-            'image' => 'public/game4.png']);
+            'image' => 'public/game4.png',
+            'quantity' => 5]);
     }
 }
