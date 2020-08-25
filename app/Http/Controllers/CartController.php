040b38d93b323ce0548cca9bfc9c6385da673e90
Diego Ospina <diego.ospinagp@gmail.com>
2020-08-24 23:34:22 -0500

    feat: added CartController and hardevine library for shopping cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Melihovv\ShoppingCart\Facades\ShoppingCart as Cart;
+//use Melihovv\ShoppingCart\Facades\ShoppingCart as Cart;
+use App\Product;
+use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\Request;
@@ -15,13 +17,35 @@ class CartController extends Controller
      */
     public function index()
     {
-        return view('store.cart');
+        $cart = Cart::instance();
+
+        return view('store.cart', [
+            'cart' => $cart
+        ]);
     }
 
     public function store(Request $request)
     {
-        Cart::add($request->id, $request->name, $request->price, 1);
+        $duplicates = Cart::search(function ($cartItem, $rowId) use ($request)
+        {
+            return $cartItem->id === $request->id;
+        });
+
+        if ($duplicates->isNotEmpty())
+        {
+            return redirect()->route('cart.index')->with('success_message', 'Ya tienes este producto en tu carrito');
+        }
+
+        Cart::add($request->id, $request->name, 1, $request->price)
+            ->associate('App\Product');
 
         return redirect()->route('cart.index')->with('success_message', 'Item añadido a tu carrito');
+    }
+
+    public function destroy($rowId)
+    {
+        Cart::remove($rowId);
+
+        return back()->with('success_message', 'Producto eliminado');
     }
 }
