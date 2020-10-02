8fb5fc47250af19bea8c11e7d16e87a8cf372b3e
Diego Ospina <diego.ospinagp@gmail.com>
2020-10-02 00:27:46 -0500

    feat: added Laravel debug bar library

--- config/app.php
+++ config/app.php
@@ -176,6 +176,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Barryvdh\Debugbar\ServiceProvider::class,
+
+
     ],
 
     /*
@@ -228,6 +231,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Cart' => Gloudemans\Shoppingcart\Facades\Cart::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
