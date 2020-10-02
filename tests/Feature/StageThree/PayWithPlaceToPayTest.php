545da446d5e8952d1bf9eea994f7fc7ae120275b
Diego Ospina <diego.ospinagp@gmail.com>
2020-10-01 19:56:38 -0500

    test: created StageThree folder for tests and PayWIthPlaceToPayTest

---
+++ tests/Feature/StageThree/PayWithPlaceToPayTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature\StageThree;
+
+use App\Library\PlaceToPayConnection;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PayWithPlaceToPayTest extends TestCase
+{
+    /** @test */
+    public function can_make_a_request_to_place_to_pay_endpoint()
+    {
+        $this->withoutExceptionHandling();
+        $response = $this->json('POST', route('checkout.placeToPayCheckout'));
+
+        $response->assertJsonStructure();
+        $response->assertJson(['status' => ['status' => 'OK']]);
+    }
+
+    /** @test */
+    public function redirect_user_to_web_checkout()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->json('POST', route('checkout.placeToPayCheckout'));
+
+        $response->assertJson(['status' => ['status' => 'OK']]);
+        $response->assertOk();
+        $this->assertAuthenticated();
+    }
+
+//    /** @test */
+    public function can_see_the_place_to_pay_transaction_status()
+    {
+
+    }
+}
