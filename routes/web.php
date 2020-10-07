6b8c07195d8103b9be9c29f2cdfb4f2092743033
Diego Ospina <diego.ospinagp@gmail.com>
2020-10-06 22:20:57 -0500

    refactor: deleted test route to destroy cart, rename route cart.remove because it makes more sense with the method

--- routes/web.php
+++ routes/web.php
@@ -22,17 +22,13 @@ Route::get('/store/{product}', 'StoreController@show')->name('store.show');
 
 Route::get('/cart', 'CartController@index')->name('cart.index');
 Route::post('/cart', 'CartController@store')->name('cart.store');
-Route::delete('/cart/{product}', 'CartController@destroy')->name('cart.destroy');
+Route::delete('/cart/{product}', 'CartController@remove')->name('cart.remove');
 Route::patch('/cart/{product}', 'CartController@update')->name('update.cart');
 
 Route::get('/checkout', 'CheckoutController@index')->name('checkout.index');
 Route::post('/checkout', 'CheckoutController@placeToPayCheckout')->name('checkout.placeToPayCheckout');
 Route::get('/success/{reference}', 'PlaceToPaySuccessController@index')->name('placeToPaySuccess.index');
 Route::get('/shoppingHistory', 'ShoppingController@index')->name('shoppingHistory.index');
-
-Route::get('/empty', function (){
-    Cart::destroy();
-});
 
 Auth::routes(['verify' => true]);
 
