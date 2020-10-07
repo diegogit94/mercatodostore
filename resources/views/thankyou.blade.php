baf3dd96980a705e780eea106e69199020324c32
Diego Ospina <diego.ospinagp@gmail.com>
2020-10-06 22:23:59 -0500

    view: added thankyou view to return home after the purchase

---
+++ resources/views/thankyou.blade.php
@@ -0,0 +1,21 @@
+@extends('layout')
+
+@section('title', 'Thank You')
+
+@section('extra-css')
+
+@endsection
+
+@section('body-class', 'sticky-footer')
+
+@section('content')
+
+   <div class="thank-you-section">
+       <h1>Thank you for <br> Your Order!</h1>
+       <div class="spacer"></div>
+       <div>
+           <a href="{{ url('/') }}" class="button">Home Page</a>
+       </div>
+   </div>
+
+@endsection
