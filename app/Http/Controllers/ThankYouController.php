d09d55f2de90f4ebfad8e026464376f068712b21
Diego Ospina <diego.ospinagp@gmail.com>
2020-11-09 12:54:34 -0500

    feat: When the purchase is "APPROVED" the product quantity is discount with the "decrement" function, added constant "STATUS_APPROVED"

--- app/Http/Controllers/ThankYouController.php
+++ app/Http/Controllers/ThankYouController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Library\PlaceToPayConnection;
 use App\Order;
+use App\Product;
 use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Http;
 
 class ThankYouController extends Controller
 {
+    public const STATUS_APPROVED = "APPROVED";
+
     public function index(string $reference)
     {
         $order = Order::where('user_id', Auth::id())
@@ -27,6 +30,13 @@ class ThankYouController extends Controller
         DB::table('orders')
         ->where('reference', getUrlReference())
             ->update(['transaction_information' => $response, 'status' => $response['status']['status']]);
+
+        if ($response['status']['status'] == self::STATUS_APPROVED && $order->status != self::STATUS_APPROVED) {
+
+            $order->products->each(function ($product) {
+                $product->decrement('quantity', $product->pivot->quantity);
+            });
+        }
 
         Cart::destroy();
 
