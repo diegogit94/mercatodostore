a0878d46bcf941013f4723a59239e5845219791f
Diego Ospina <diego.ospinagp@gmail.com>
2020-11-10 14:36:27 -0500

    feat: added new view for admin to manage the export/import

---
+++ resources/views/admin/manageAdminInfo.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+    <!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MercaTodo</title>
+
+    </head>
+    <body>
+        <div class="container">
+            <h2>Usuarios</h2>
+            <p class="lead">This example is a quick exercise to illustrate how the bottom navbar works.</p>
+            <a class="btn btn-mid btn-success" href="{{ route('users.export') }}" role="button">Exportar ⇩</a>
+            <a class="btn btn-mid btn-success" href="#" role="button">Importar ⇧</a>
+        </div>
+        <div class="container">
+            <br><h2>Productos</h2>
+            <p class="lead">This example is a quick exercise to illustrate how the bottom navbar works.</p>
+            <a class="btn btn-mid btn-success" href="#" role="button">Exportar ⇩</a>
+            <a class="btn btn-mid btn-success" href="#" role="button">Importar ⇧</a>
+        </div>
+        <div class="container">
+            <br><h2>Ordenes</h2>
+            <p class="lead">This example is a quick exercise to illustrate how the bottom navbar works.</p>
+            <a class="btn btn-mid btn-success" href="#" role="button">Exportar ⇩</a>
+            <a class="btn btn-mid btn-success" href="#" role="button">Importar ⇧</a>
+        </div>
+    </body>
+</html>
+@endsection
