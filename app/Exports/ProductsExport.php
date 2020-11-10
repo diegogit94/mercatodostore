249e161e82c1d5833496bb804ae351ff12be4437
Diego Ospina <diego.ospinagp@gmail.com>
2020-11-10 15:51:45 -0500

    feat: configured the ProductsExport class to establish a format on file

--- app/Exports/ProductsExport.php
+++ app/Exports/ProductsExport.php
@@ -4,14 +4,54 @@ namespace App\Exports;
 
 use App\Product;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithDrawings;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use PhpOffice\PhpSpreadsheet\Worksheet\BaseDrawing;
+use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
 
-class ProductsExport implements FromCollection
+class ProductsExport implements FromCollection, ShouldAutoSize, WithDrawings, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-        return Product::all();
+        return Product::select([
+            'id',
+            'name',
+            'description',
+            'short_description',
+            'image',
+            'price',
+            'quantity',
+            'visible',
+        ])->get();
+    }
+
+    public function drawings()
+    {
+        $drawing = new Drawing();
+        $drawing->setName('Logo');
+        $drawing->setDescription('This is my logo');
+        $drawing->setPath(public_path('/img/store-logo.png'));
+        $drawing->setHeight(60);
+        $drawing->setCoordinates('J2');
+
+        return $drawing;
+    }
+
+    public function headings(): array
+    {
+        return ([
+            'id',
+            'name',
+            'description',
+            'short_description',
+            'image',
+            'price',
+            'quantity',
+            'visible',
+        ]);
     }
 }
