11d7ba0f70428c061bbd6c1bcb6e8cb91af05305
Diego Ospina <diego.ospinagp@gmail.com>
2020-11-24 05:57:41 -0500

    feat: created the export notification

---
+++ app/Notifications/ExportFinished.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ExportFinished extends Notification
+{
+    use Queueable;
+
+    public $filePath;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param $filePath
+     */
+    public function __construct($filePath)
+    {
+        $this->filePath = $filePath;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('Link de tu exportación')
+                    ->action('Descargar', url($this->filePath))
+                    ->line(';)');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
