e7cd24df87cf3d662188000e4ec2ba2943456410
Diego Ospina <diego.ospinagp@gmail.com>
2020-11-26 22:47:41 -0500

    feat: added API products index route

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ use Illuminate\Support\Facades\Route;
 //});
 
 Route::get('products/{product}', 'ProductController@show')->name('api.v1.products.show');
+Route::get('products', 'ProductController@index')->name('api.v1.products.index');
