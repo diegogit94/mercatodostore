0a2dd73d3a254be8bcd7d50d085ec558af4a74e3
Diego Ospina <diego.ospinagp@gmail.com>
2020-11-26 22:51:07 -0500

    feat: created ProductCollection to and added index method to ProductController

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductCollection;
+use App\Http\Resources\ProductResource;
 use App\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    public function index()
+    {
+        return ProductCollection::make(Product::All());
+    }
+
     public function show(Product $product)
     {
         return ProductResource::make($product);
