85274c4fcea7e90e82a7f684ca2f7f87f1928224
Diego Ospina <diego.ospinagp@gmail.com>
2020-11-26 22:46:05 -0500

    feat(test): created test to fetch all the products

--- tests/Feature/Api/Products/ListProductTest.php
+++ tests/Feature/Api/Products/ListProductTest.php
@@ -9,6 +9,8 @@ use Tests\TestCase;
 
 class ListProductTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
     public function can_fetch_single_product()
     {
@@ -22,13 +24,67 @@ class ListProductTest extends TestCase
                 'type' => 'products',
                 'id' => (string) $product->getRouteKey(),
                 'attributes' => [
-                    'title' => $product->name,
+                    'name' => $product->name,
                     'slug' => $product->slug,
                     'description' => $product->description,
                 ],
                 'links' => [
                     'self' => route('api.v1.products.show', $product)
                 ]
+            ]
+        ]);
+    }
+
+    /** @test */
+    public function can_fetch_all_articles()
+    {
+        $this->withoutExceptionHandling();
+
+        $products = factory(Product::class)->times(3)->create();
+
+        $response = $this->getJson(route('api.v1.products.index'));
+
+        $response->assertExactJson([
+            'data' => [
+                [
+                'type' => 'products',
+                'id' => (string) $products[0]->getRouteKey(),
+                'attributes' => [
+                    'name' => $products[0]->name,
+                    'slug' => $products[0]->slug,
+                    'description' => $products[0]->description,
+                ],
+                'links' => [
+                    'self' => route('api.v1.products.show', $products[0])
+                    ]
+                ],
+                [
+                'type' => 'products',
+                'id' => (string) $products[1]->getRouteKey(),
+                'attributes' => [
+                    'name' => $products[1]->name,
+                    'slug' => $products[1]->slug,
+                    'description' => $products[1]->description,
+                ],
+                'links' => [
+                    'self' => route('api.v1.products.show', $products[1])
+                    ]
+                ],
+                [
+                'type' => 'products',
+                'id' => (string) $products[2]->getRouteKey(),
+                'attributes' => [
+                    'name' => $products[2]->name,
+                    'slug' => $products[2]->slug,
+                    'description' => $products[2]->description,
+                ],
+                'links' => [
+                    'self' => route('api.v1.products.show', $products[2])
+                    ]
+                ],
+            ],
+            'links' => [
+                'self' => route('api.v1.products.index')
             ]
         ]);
     }
