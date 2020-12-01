4a0bb6d0c96b37a1ec8681b0ca96e97af4902314
Diego Ospina <diego.ospinagp@gmail.com>
2020-12-01 10:49:23 -0500

    fix: changed de Product queries to improve the advanced search

--- app/Product.php
+++ app/Product.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Laravel\Scout\Searchable;
@@ -36,28 +37,40 @@ class Product extends Model
         return $this->update(['visible' => !$this->visible]);
     }
 
-    public function scopeName($query, $name)
+    public function scopeName(Builder $query, string $name = null): Builder
     {
-        if ($name)
+        if ($name) {
             return $query->where('name', 'LIKE', "%$name%");
+        }
+
+        return $query;
     }
 
-    public function scopePrice($query, $price)
+    public function scopePrice(Builder $query, float $price = null): Builder
     {
-        if ($price)
-            return $query->where('price', 'LIKE', "%$price%");
+        if ($price) {
+            return $query->where('price', $price);
+        }
+
+        return $query;
     }
 
-    public function scopeVisible($query, $visible)
+    public function scopeVisible(Builder $query, string $visible = null): Builder
     {
-        if ($visible)
-            return $query->where('visible', '=' , $visible);
+        if ($visible) {
+            return $query->where('visible', $visible === 'true');
+        }
+
+        return $query;
     }
 
-    public function scopeCategory($query, $category)
+    public function scopeCategory(Builder $query, int $category = null): Builder
     {
-        if ($category)
-            return $query->where('category_id', 'LIKE', "%$category%");
+        if ($category) {
+            return $query->where('category_id', $category);
+        }
+
+        return $query;
     }
 
     public function orders(): BelongsToMany
