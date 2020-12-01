f344d89b9d38b65ace2fe179764701e39438d602
Diego Ospina <diego.ospinagp@gmail.com>
2020-12-01 13:47:37 -0500

    feat: created sort options with query

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -7,12 +7,28 @@ use App\Http\Resources\ProductCollection;
 use App\Http\Resources\ProductResource;
 use App\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        return ProductCollection::make(Product::All());
+        $sortFields = Str::of(request('sort'))->explode(',');
+        $productQuery = Product::query();
+
+        foreach ($sortFields as $sortField)
+        {
+            $direction = 'asc';
+
+            if (Str::of($sortField)->startsWith('-'))
+            {
+                $direction = 'desc';
+                $sortField = Str::of($sortField)->substr(1);
+            }
+            $productQuery->orderBy($sortField, $direction);
+        }
+
+        return ProductCollection::make($productQuery->get());
     }
 
     public function show(Product $product)
