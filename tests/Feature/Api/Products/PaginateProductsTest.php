5fe937e9c4d3966d2ab87b44470ef1e82abfbb52
Diego Ospina <diego.ospinagp@gmail.com>
2020-12-02 15:33:02 -0500

    test: created test to paginate products

--- tests/Feature/Api/Products/PaginateProductsTest.php
+++ tests/Feature/Api/Products/PaginateProductsTest.php
@@ -20,6 +20,31 @@ class PaginateProductsTest extends TestCase
 
         $response = $this->getJson($url);
 
-        $response->assertJsonCount(2, 'data');
+        $response->assertJsonCount(2, 'data')
+            ->assertDontSee($products[0]->name)
+            ->assertDontSee($products[1]->name)
+            ->assertDontSee($products[2]->name)
+            ->assertDontSee($products[3]->name)
+            ->assertSee($products[4]->name)
+            ->assertSee($products[5]->name)
+            ->assertDontSee($products[6]->name)
+            ->assertDontSee($products[7]->name)
+            ->assertDontSee($products[8]->name)
+            ->assertDontSee($products[9]->name);
+
+//        foreach ($response->json('links') as $link) {
+//            dump(urldecode($link));
+//        }
+
+        $response->assertJsonStructure([
+            'links' => ['first', 'last', 'prev', 'next']
+        ]);
+
+        $response->assertJsonFragment([
+            'first' => route('api.v1.products.index', ['page[size]' => 2, 'page[number]' => 1]),
+            'last' => route('api.v1.products.index', ['page[size]' => 2, 'page[number]' => 5]),
+            'prev' => route('api.v1.products.index', ['page[size]' => 2, 'page[number]' => 2]),
+            'next' => route('api.v1.products.index', ['page[size]' => 2, 'page[number]' => 4]),
+        ]);
     }
 }
