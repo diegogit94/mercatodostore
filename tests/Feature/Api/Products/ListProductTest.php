489ce5e3c871d180938a26c281649586da342ca8
Diego Ospina <diego.ospinagp@gmail.com>
2020-12-02 17:10:00 -0500

    test: added more attributes to show on API index

--- tests/Feature/Api/Products/ListProductTest.php
+++ tests/Feature/Api/Products/ListProductTest.php
@@ -27,6 +27,11 @@ class ListProductTest extends TestCase
                     'name' => $product->name,
                     'slug' => $product->slug,
                     'description' => $product->description,
+                    'image' => $product->image,
+                    'price' => $product->price,
+                    'quantity' => $product->quantity,
+                    'visible' => $product->visible,
+//                    'category' => $product->category_id,
                 ],
                 'links' => [
                     'self' => route('api.v1.products.show', $product)
@@ -53,6 +58,11 @@ class ListProductTest extends TestCase
                     'name' => $products[0]->name,
                     'slug' => $products[0]->slug,
                     'description' => $products[0]->description,
+                    'image' => $products[0]->image,
+                    'price' => $products[0]->price,
+                    'quantity' => $products[0]->quantity,
+                    'visible' => $products[0]->visible,
+//                    'category' => $products[0]->category_id,
                 ],
                 'links' => [
                     'self' => route('api.v1.products.show', $products[0])
@@ -65,6 +75,11 @@ class ListProductTest extends TestCase
                     'name' => $products[1]->name,
                     'slug' => $products[1]->slug,
                     'description' => $products[1]->description,
+                    'image' => $products[1]->image,
+                    'price' => $products[1]->price,
+                    'quantity' => $products[1]->quantity,
+                    'visible' => $products[1]->visible,
+//                    'category' => $products[1]->category_id,
                 ],
                 'links' => [
                     'self' => route('api.v1.products.show', $products[1])
@@ -77,6 +92,11 @@ class ListProductTest extends TestCase
                     'name' => $products[2]->name,
                     'slug' => $products[2]->slug,
                     'description' => $products[2]->description,
+                    'image' => $products[2]->image,
+                    'price' => $products[2]->price,
+                    'quantity' => $products[2]->quantity,
+                    'visible' => $products[2]->visible,
+//                    'category' => $products[2]->category_id,
                 ],
                 'links' => [
                     'self' => route('api.v1.products.show', $products[2])
