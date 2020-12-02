27ecc699d612c2fd540a4d908cdd886775301e1c
Diego Ospina <diego.ospinagp@gmail.com>
2020-12-02 16:15:46 -0500

    refactor: deleted the old macros on the service providers and replaced with the new mixin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Providers\JsonApi\JsonApiBuilder;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\ServiceProvider;
 use App\Product;
@@ -32,42 +33,6 @@ class AppServiceProvider extends ServiceProvider
         Product::observe(ProductObserver::class);
         Category::observe((CategoryObserver::class));
 
-        Builder::macro('jsonPaginate', function () {
-            return $this->paginate(
-                $perPage = request('page.size'),
-                $columns = ['*'],
-                $pageName = 'page[number]',
-                $page = request('page.number')
-            )->appends(request()->except('page.number'));
-        });
-
-        Builder::macro('applySorts', function () {
-            if ( ! property_exists($this->model, 'allowedSorts')) {
-                abort (500, 'Please, set the public property "$allowedSorts" inside ' . get_class($this->model));
-            }
-
-            if (is_null($sort = request('sort'))) {
-                return $this;
-            }
-
-            $sortFields = Str::of($sort)->explode(',');
-
-            foreach ($sortFields as $sortField)
-            {
-                $direction = 'asc';
-
-                if (Str::of($sortField)->startsWith('-')) {
-                    $direction = 'desc';
-                    $sortField = Str::of($sortField)->substr(1);
-                }
-
-                if ( ! collect($this->model->allowedSorts)->contains($sortField)) {
-                    abort(400, "Invalid query parameter, {$sortField} is not allowed");
-                }
-
-                $this->orderBy($sortField, $direction);
-            }
-            return $this;
-        });
+        Builder::mixin(new JsonApiBuilder);
     }
 }
