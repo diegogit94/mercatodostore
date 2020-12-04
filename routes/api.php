fcef69eaa1b5f7962e3ebdd79f7b8471a7b20ae5
Diego Ospina <diego.ospinagp@gmail.com>
2020-12-04 13:25:17 -0500

    feat: added API package resource route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use CloudCreativity\LaravelJsonApi\Facades\JsonApi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,5 +19,9 @@ use Illuminate\Support\Facades\Route;
 //    return $request->user();
 //});
 
-Route::get('products/{product}', 'ProductController@show')->name('api.v1.products.show');
-Route::get('products', 'ProductController@index')->name('api.v1.products.index');
+//Route::get('products/{product}', 'ProductController@show')->name('api.v1.products.show');
+//Route::get('products', 'ProductController@index')->name('api.v1.products.index');
+
+JsonApi::register('v1')->routes( function($api) {
+    $api->resource('products');
+});
