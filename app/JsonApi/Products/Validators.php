3e9630adee67d4ebd16113393ef433d6e402f397
Diego Ospina <diego.ospinagp@gmail.com>
2020-12-04 13:01:11 -0500

    feat: added allowed sort and filtering parameters to API's validators file

--- app/JsonApi/Products/Validators.php
+++ app/JsonApi/Products/Validators.php
@@ -21,7 +21,7 @@ class Validators extends AbstractValidators
      * @var string[]|null
      *      the allowed fields, an empty array for none allowed, or null to allow all fields.
      */
-    protected $allowedSortParameters = [];
+    protected $allowedSortParameters = ['name', 'description'];
 
     /**
      * The filters a client is allowed send.
@@ -29,7 +29,7 @@ class Validators extends AbstractValidators
      * @var string[]|null
      *      the allowed filters, an empty array for none allowed, or null to allow all.
      */
-    protected $allowedFilteringParameters = [];
+    protected $allowedFilteringParameters = ['search', 'name', 'description', 'price', 'month', 'year'];
 
     /**
      * Get resource validation rules.
