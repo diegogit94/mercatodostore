1a429a4dc3d2e4c2a8291f0e5999f3587844c0e7
Diego Ospina <diego.ospinagp@gmail.com>
2020-12-04 12:55:19 -0500

    feat: added guarded field with id attribute to API adapter

--- app/JsonApi/Products/Adapter.php
+++ app/JsonApi/Products/Adapter.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Collection;
 
 class Adapter extends AbstractAdapter
 {
+    protected $guarded = ['id'];
 
     /**
      * Mapping of JSON API attribute field names to model keys.
