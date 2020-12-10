fc533e0f5340b9763c0070163fdbcc42781390cf
Diego Ospina <diego.ospinagp@gmail.com>
2020-12-10 00:54:34 -0500

    test: added API test to filter users by email

--- tests/Feature/Api/Users/FilterUsersTest.php
+++ tests/Feature/Api/Users/FilterUsersTest.php
@@ -36,6 +36,34 @@ class FilterUsersTest extends TestCase
     }
 
     /** @test */
+    public function can_filter_users_by_email()
+    {
+        factory(User::class)->create([
+            'name' => 'User A',
+            'email' => 'test1@mercatodo.com'
+        ]);
+
+        factory(User::class)->create([
+            'name' => 'User B',
+            'email' => 'test2@mercatodo.com'
+        ]);
+
+        factory(User::class)->create([
+            'name' => 'User C',
+            'email' => 'test@tienda.com'
+
+        ]);
+
+        $url = route('api.v1.users.index', ['filter[email]' => 'mercatodo']);
+
+        $this->jsonApi()->get($url)
+            ->assertJsonCount(2, 'data')
+            ->assertSee('User A')
+            ->assertSee('User B')
+            ->assertDontSee('User C');
+    }
+
+    /** @test */
     public function can_filter_users_by_active()
     {
         factory(User::class)->create([
@@ -61,5 +89,26 @@ class FilterUsersTest extends TestCase
             ->assertDontSee('User B')
             ->assertDontSee('User C');
 
+    }
+
+    /** @test */
+    public function cannot_filter_users_by_unknown_parameter()
+    {
+        factory(User::class)->create([
+            'name' => 'User A',
+        ]);
+
+        factory(User::class)->create([
+            'name' => 'User B',
+        ]);
+
+        factory(User::class)->create([
+            'name' => 'User C',
+        ]);
+
+        $url = route('api.v1.users.index', ['filter[unknown]' => 'value']);
+
+        $this->jsonApi()->get($url)
+            ->assertStatus(400);
     }
 }
