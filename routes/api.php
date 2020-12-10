68070157afaa403eccf7e72997fd410e0ea12c3b
Diego Ospina <diego.ospinagp@gmail.com>
2020-12-10 16:21:34 -0500

    feat: added admin middleware on products and users API routes

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use CloudCreativity\LaravelJsonApi\Facades\JsonApi;
 
 JsonApi::register('v1')->routes( function($api) {
-    $api->resource('products')->only('create', 'update', 'delete')->middleware('auth');
+    $api->resource('products')->only('create', 'update', 'delete')->middleware('admin');
     $api->resource('products')->except('create', 'update', 'delete');
-    $api->resource('users')->middleware('auth');
+    $api->resource('users')->middleware('admin');
 });
