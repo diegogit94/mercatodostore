31a051921ae6dd068dc7561f40862542f00f1973
Diego Ospina <diego.ospinagp@gmail.com>
2020-12-09 19:44:02 -0500

    feat: added API's resource route for users

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,5 @@ use CloudCreativity\LaravelJsonApi\Facades\JsonApi;
 JsonApi::register('v1')->routes( function($api) {
     $api->resource('products')->only('create', 'update', 'delete')->middleware('auth');
     $api->resource('products')->except('create', 'update', 'delete');
+    $api->resource('users')->only('index', 'read');
 });
