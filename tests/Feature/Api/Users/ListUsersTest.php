881b5c6e52e90da4d36de22bd6a4c9a790c49ca8
Diego Ospina <diego.ospinagp@gmail.com>
2020-12-09 19:43:19 -0500

    test: created test to fetch users

---
+++ tests/Feature/Api/Users/ListUsersTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature\Api\Users;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ListUsersTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function can_fetch_all_user()
+    {
+        $users = factory(User::class)->times(3)->create();
+
+        $this->jsonApi()->get(route('api.v1.users.index'))
+            ->assertSee($users[0]->name)
+            ->assertSee($users[0]->email)
+            ->assertSee($users[0]->user_type)
+            ->assertSee($users[0]->active);
+    }
+
+    /** @test */
+    public function can_fetch_a_single_users()
+    {
+        $user = factory(User::class)->create();
+
+        $this->jsonApi()->get(route('api.v1.users.read', $user))
+            ->assertSee($user['name'])
+            ->assertSee($user['email'])
+            ->assertSee($user['user_type'])
+            ->assertSee($user['active']);
+    }
+}
