ad42c4ebe804e377dd9770db32fbb4db6df9d752
Diego Ospina <diego.ospinagp@gmail.com>
2020-12-11 22:31:28 -0500

    test: added actingAs to ListUsersTest

--- tests/Feature/Api/Users/ListUsersTest.php
+++ tests/Feature/Api/Users/ListUsersTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Api\Users;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class ListUsersTest extends TestCase
@@ -15,6 +16,8 @@ class ListUsersTest extends TestCase
     public function can_fetch_all_user()
     {
         $users = factory(User::class)->times(3)->create();
+
+        Sanctum::actingAs(factory(User::class)->create(['user_type' => 'admin']));
 
         $this->jsonApi()->get(route('api.v1.users.index'))
             ->assertSee($users[0]->name)
