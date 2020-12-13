d7d5b93bd85105fa95f507920b7528214ea57a03
Diego Ospina <diego.ospinagp@gmail.com>
2020-12-12 19:29:32 -0500

    feat: edited ProductPolicy with all the methods and the deny messagess

--- app/Policies/ProductPolicy.php
+++ app/Policies/ProductPolicy.php
@@ -5,22 +5,42 @@ namespace App\Policies;
 use App\Product;
 use App\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
 
 class ProductPolicy
 {
     use HandlesAuthorization;
 
-    public function create(User $user)
+    /**
+     * @param User $user
+     * @return Response
+     */
+    public function create(User $user): Response
     {
-        return $user->user_type === 'admin';
+        return $user->user_type === 'admin'
+            ? Response::allow()
+            : Response::deny('Not admin, You shall not pass!');
     }
 
+    /**
+     * @param User $user
+     * @return Response
+     */
     public function update(User $user)
     {
-        return $user->user_type === 'admin';
+        return $user->user_type === 'admin'
+            ? Response::allow()
+            : Response::deny('Not admin, You shall not pass!');
     }
+
+    /**
+     * @param User $user
+     * @return Response
+     */
     public function delete(User $user)
     {
-        return $user->user_type === 'admin';
+        return $user->user_type === 'admin'
+            ? Response::allow()
+            : Response::deny('Not admin, You shall not pass!');
     }
 }
