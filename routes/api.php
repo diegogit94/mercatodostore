74afd9c01124b3104e60dd2e578911ce415252f4
Diego Ospina <diego.ospinagp@gmail.com>
2020-12-12 19:28:40 -0500

    fix: changed middleware admin to auth:sanctum middleware, the access control to admin routes will be with policies

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use CloudCreativity\LaravelJsonApi\Facades\JsonApi;
 
 JsonApi::register('v1')->routes( function($api) {
-    $api->resource('products')->only('create', 'update', 'delete')->middleware('admin');
+    $api->resource('products')->only('create', 'update', 'delete')->middleware('auth:sanctum');
     $api->resource('products')->except('create', 'update', 'delete');
-    $api->resource('users')->middleware('admin');
+    $api->resource('users')->middleware('auth:sanctum');
 });
